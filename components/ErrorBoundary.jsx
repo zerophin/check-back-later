9f72c4496b057c98c0ec3d20add5f6d964d022ea
Randal Smith <randalj@protonmail.com>
2021-09-09 16:08:13 -0500

    added error boundary

---
+++ components/ErrorBoundary.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // You can also log the error to an error reporting service
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return (
+        <>
+          <h1>Something went wrong.</h1>
+          <p>
+            Clear local storage will likely fix the problem{" "}
+            <button
+              onClick={() => {
+                localStorage.clear();
+                location.reload();
+              }}
+            >
+              clear
+            </button>
+          </p>
+        </>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+export default ErrorBoundary;
