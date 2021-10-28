64854adeeecfcf7151fe1ce9d9fe194baa82ebe8
Randal Smith <randalj@protonmail.com>
2021-10-28 17:43:04 -0500

    feature: added sort button in Stories component

--- components/Stories.jsx
+++ components/Stories.jsx
@@ -1,5 +1,6 @@
 import styles from "../styles/Home.module.css";
 import Story from "./Story";
+import { useState } from "react";
 
 function Stories({
   stories,
@@ -8,20 +9,40 @@ function Stories({
   updateStoryComments,
   buttonText = "delete",
 }) {
+  const [sorted, setSorted] = useState(false);
+
   const isValidStory = (story) => story && story.type === "story";
+  const sortByCommentCount = (a, b) => {
+    const aNewComments = a.props.post.descendants - a.props.previousCount;
+    const bNewComments = b.props.post.descendants - b.props.previousCount;
+    return aNewComments - bNewComments;
+  };
+
+  const storyList = stories.filter(isValidStory).map((story, i) => {
+    let previousCount = list[i] && list[i][1];
+    return (
+      <Story
+        key={story.id}
+        post={story}
+        handleClick={updateStoryComments}
+        handleButtonClick={handleButtonClick}
+        previousCount={previousCount}
+        buttonText={buttonText}
+      />
+    );
+  });
+
   return (
-    <ol className={styles.list}>
-      {stories.filter(isValidStory).map((story, i) => (
-        <Story
-          key={story.id}
-          post={story}
-          handleButtonClick={handleButtonClick}
-          previousCount={list[i] && list[i][1]}
-          handleClick={updateStoryComments}
-          buttonText={buttonText}
-        />
-      ))}
-    </ol>
+    <>
+      {updateStoryComments && (
+        <button onClick={() => setSorted(!sorted)}>
+          {sorted ? `Sort by added` : `Sort by new comments`}
+        </button>
+      )}
+      <ol className={styles.list}>
+        {sorted ? storyList.sort(sortByCommentCount) : storyList}
+      </ol>
+    </>
   );
 }
 
